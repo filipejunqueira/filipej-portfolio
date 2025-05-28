6e7926194b38088f67a274f7f764d5cd7dd6a318
filipejunqueira <filipelqj@gmail.com>
2025-05-28 10:19:24 -0300

    Changing vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/filipej-portfolio/", // Crutial for GitHub Pages deployment
+  base: "/", // Crutial for GitHub Pages deployment
 });
